989a28d7dda34e1cd6e79c96355cbc890689e29c
Shubham Kumar <sky143c@gmail.com>
2026-02-06 03:31:12 +0530

    first commit

---
+++ backend/config/mongodb.js
@@ -0,0 +1,20 @@
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+const connectDB = async () => {
+  try {
+    mongoose.connection.on("connected", () => console.log("✅ Database Connected"));
+    mongoose.connection.on("error", (err) => console.error("❌ DB Error:", err));
+
+    await mongoose.connect(process.env.MONGODB_URI, {
+      dbName: "CareMate",
+    });
+  } catch (err) {
+    console.error("❌ Failed to connect to DB", err);
+    process.exit(1);
+  }
+};
+
+export default connectDB;
